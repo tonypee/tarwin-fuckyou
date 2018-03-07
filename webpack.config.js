296ab0f41363cb2533255d67b3a720a7c13341a3
Tony Polinelli <tonypolinelli@gmail.com>
2018-03-07 12:36:43 +1100

    remove sourcemaps from production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  devtool: "inline-source-map",
+  devtool: "eval",
   entry: "./src/index.tsx",
   output: {
     filename: "bundle.js",
