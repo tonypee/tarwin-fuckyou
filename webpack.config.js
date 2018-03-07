c2558ad77a3b0bed864e7ace9c89c7a0fcca0b35
Tony Polinelli <tonypolinelli@gmail.com>
2018-03-07 12:50:17 +1100

    .ts not .tsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   devtool: "eval",
-  entry: "./src/index.tsx",
+  entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
